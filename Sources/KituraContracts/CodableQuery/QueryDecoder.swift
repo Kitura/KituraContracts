8580621a030a6e142063198fb78b40218764e38a
Cameron McWilliam <cam.mcwilliam@me.com>
2019-09-30 14:57:08 +0100

    Added not linux condition for @unknown default

--- Sources/KituraContracts/CodableQuery/QueryDecoder.swift
+++ Sources/KituraContracts/CodableQuery/QueryDecoder.swift
@@ -211,7 +211,7 @@ public class QueryDecoder: Coder, Decoder, BodyDecoder {
                 return try decodeType(fieldValue?.date(formatted), to: T.self)
             case .custom(let closure):
                 return try decodeType(closure(self), to: T.self)
-            #if swift(>=5)
+            #if swift(>=5) && !os(Linux)
             @unknown default:
                 throw DateError.unknownStrategy
             #endif
